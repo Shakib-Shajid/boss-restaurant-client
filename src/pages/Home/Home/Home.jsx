16e65279b450193c649bd47bf13e17ebb710faff
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-11-16 14:40:32 +0600

    custom hook added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,15 +1,15 @@
-import PopularMenu from "../../PopularMenu/PopularMenu";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import Testimonials from "../Testimonials/Testimonials";
-
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
             <Helmet>
-                <title>Boss | Home</title>
+                <title>Boss | Home</title>      {/*change title*/}
             </Helmet>
             <Banner></Banner>
             <Category></Category>
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from "../../../assets/menu/banner3.jpg"
+
 
 const Menu = () => {
     return (
@@ -7,7 +10,8 @@ const Menu = () => {
             <Helmet>
                 <title>Boss | Menu</title>
             </Helmet>
-            <h2>menu page</h2>
+            <Cover img={menuImg} title="our menu"></Cover>
+
         </div>
     );
 };
