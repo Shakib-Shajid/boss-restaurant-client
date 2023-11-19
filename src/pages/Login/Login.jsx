2be000216eb5523b911a30b4608e85169ab0b065
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-11-19 13:26:10 +0600

    signup, login, logout added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Login = () => {
 
     const captchaRef = useRef(null);
     const [disable, setDisable] = useState(true);
+
+    const { signIn } = useContext(AuthContext);    //use provider
+
     useEffect(() => {
         loadCaptchaEnginge(6);
     }, [])
@@ -16,57 +20,64 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
-    }
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.log(error)
+            })
 
-    const handleValidateCaptcha = () => {
-        const user_capture_value = captchaRef.current.value;
-        if (validateCaptcha(user_capture_value)) {
-            setDisable(false);
+        const handleValidateCaptcha = () => {
+            const user_capture_value = captchaRef.current.value;
+            if (validateCaptcha(user_capture_value)) {
+                setDisable(false);
+            }
+            else {
+                setDisable(true);
+            }
         }
-        else {
-            setDisable(true);
-        }
-    }
-    return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center md:w-1/2 lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                </div>
-                <div className="card shrink-0 w-full md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body" onSubmit={handleLogin}>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <LoadCanvasTemplate />
-                            </label>
-                            <input type="text" ref={captchaRef} placeholder="type the text above" name='captcha' className="input input-bordered" required />
-                            <button onClick={handleValidateCaptcha} className='btn btn-outline btn-xs m-2'>Validate</button>
+        return (
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center md:w-1/2 lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card shrink-0 w-full md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body" onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <LoadCanvasTemplate />
+                                </label>
+                                <input type="text" ref={captchaRef} placeholder="type the text above" name='captcha' className="input input-bordered" required />
+                                <button onClick={handleValidateCaptcha} className='btn btn-outline btn-xs m-2'>Validate</button>
 
-                        </div>
-                        <div className="form-control mt-6">
-                            <input disabled={disable} type="submit" value="Login" className="btn btn-primary" />
-                        </div>
-                    </form>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input disabled={disable} type="submit" value="Login" className="btn btn-primary" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
 
-export default Login;
+    export default Login;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,9 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
@@ -11,9 +12,40 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    // create user
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    // sign in
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    // log out
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+
+    }
+
+    // outside api hit -> will subscribe one things
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
     const authInfo = {
         user,
         loading,
+        createUser,
+        signIn,
+        logOut,
     }
     return (
         <AuthContext.Provider value={authInfo}>
