c8fb95325e1cf8f92fc4309f6fa8d37fbc45531b
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-12-26 20:51:25 +0600

    going to update this project again

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './providers/AuthProvider';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
-    </AuthProvider>
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
