af9e7caa03bf400b6d7aefbc3ac44c3e12b4eab9
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-01-03 22:25:09 +0600

    server added

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -5,7 +5,7 @@ const FoodCard = ({ item }) => {
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
             <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4">${price}</p>
-            <div className="card-body text-center">
+            <div className="card-body flex flex-col items-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
