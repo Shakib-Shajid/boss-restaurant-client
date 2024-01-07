8bb588453d7b786ee56b99bbfa2bb70a38045f15
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-01-07 13:19:33 +0600

    add to cart added by axios

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,6 +1,62 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
 
 const FoodCard = ({ item }) => {
-    const { name, image, price, recipe } = item;
+    const { name, image, price, recipe, _id } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleAddToCart = food => {
+        if (user && user.email) {
+            // send cart to database
+            console.log(user.email, food)
+
+            const cartItem = {
+                menuId: _id,
+                email: user.email,
+                name,
+                image,
+                price
+            }
+
+            axios.post('http://localhost:5000/carts', cartItem)
+                .then(res => {
+                    console.log(res.data)
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `${name} added to your card`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: "You are not Logged In",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // send the user to the login page
+                    navigate('/login', { state: { from: location } });
+                }
+            });
+        }
+    }
+
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -9,7 +65,9 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+                    <button
+                        onClick={() => handleAddToCart(item)}
+                        className="btn btn-outline border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
                 </div>
             </div>
         </div>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa"
 
 const Navbar = () => {
 
@@ -18,9 +19,17 @@ const Navbar = () => {
             <li><Link to="/menu">Our Menu</Link></li>
             <li><Link to="/order/salad">Order Food</Link></li>
             <li><Link to="/secret">Secret</Link></li>
+            <li>
+                <Link to="">
+                    <button className="btn">
+                        <FaShoppingCart className="mr-2" />
+                        <div className="badge badge-secondary">+0</div>
+                    </button>
+                </Link>
+            </li>
             {
                 user ? <>
-                    <span>{user?.displayName}</span>
+                    {/* <span>{user?.displayName}</span> */}
                     <button onClick={handleLogOut} className="btn btn-ghost">Logout</button>
                 </> : <>
                     <li><Link to="/login">Login</Link></li>
